c296b39a20fe3cf6a823a37849ddeeeae6bbc652
jacob5948 <jacobe5948@outlook.com>
2026-03-25 07:41:06 -0500

    Fix build.sh to use bash for sub-scripts (avoid permission issues)

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cd spotupnp && ./build.sh $1 $2 $3 && cd ..
-cd spotraop && ./build.sh $1 $2 $3 && cd ..
+cd spotupnp && bash build.sh $1 $2 $3 && cd ..
+cd spotraop && bash build.sh $1 $2 $3 && cd ..
